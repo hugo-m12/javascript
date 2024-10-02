2286acb1a090ccd44ba5c2ee77d5302a1fc36513
Hugo Moreira <hugo.m@live.com.pt>
2024-10-02 19:11:38 +0100

    deleted var for the sprite

--- jsweb-pokemonfetchdatahw/scripts/main.js
+++ jsweb-pokemonfetchdatahw/scripts/main.js
@@ -7,19 +7,17 @@
 
 
             response.json().then(function (body) {
-                
-                const sprite = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/258.png";
-
                 loading.style.display = "none";
                 pokemonList.innerHTML += `<h1> POKEMON# </h1>`;
                 pokemonList.innerHTML += `<h2 style="color:red">${body.id}</h2>`;
-                pokemonList.innerHTML += `<img src="${sprite}" alt="pokemon_sprite">`;
+                pokemonList.innerHTML += `<img src="${body.sprites.other[`official-artwork`].front_default}" alt="pokemon_sprite">`;
                 pokemonList.innerHTML += `<h3 style="text-transform: uppercase; font-size: xx-large;">${body.name}</h3>`;
                 pokemonList.innerHTML += `<li> Weight: ${body.weight} </li>`;
                 pokemonList.innerHTML += `<li> Height: ${body.height} </li>`;
                 pokemonList.innerHTML += `Cry:<input type="button" value="▶️" onclick="playMusic()" /> `;
                 pokemonList.innerHTML += `<li style="text-transform: capitalize; color: #4592c4;"> Type: ${body.types[0].type.name} </li>`;
             });
+
             
         });
         function playMusic(){
